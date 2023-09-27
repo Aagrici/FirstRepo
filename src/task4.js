ebeb234ea180296f60ec2dc12239732d3cb173c3
Agrici Alina <alina.agrici@endava.com>
2023-09-27 18:05:05 +0300

    add task1 , 2, 3, 4, 5

---
+++ src/task4.js
@@ -0,0 +1,14 @@
+
+let x = 5;
+let y = 5;
+let z = 13;
+
+
+if(x==y && z>x+y){
+    console.log("It is true!")
+}
+else{
+    console.log("It is false!")
+}
+   
+
---
+++ src/task5.js
@@ -0,0 +1,8 @@
+
+let age = 11;
+let accompanied = true;
+
+
+if(age>11 || accompanied == true){
+    console.log("Let him in!")
+} else {"He is not allowed!"}
