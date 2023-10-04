6ff611588b93f8dadfd4f332d6f35ccc08fd468f
Agrici Alina <alina.agrici@endava.com>
2023-10-04 17:18:40 +0300

    add 1 file; 2 updated

---
+++ src/bc2.js
@@ -0,0 +1,11 @@
+let favMovies = ['A beautiful mind', 'The Shawshank redemption', 'Captain Fantastic', 'Million Dollar Baby', 
+                'The Bucket List', 'Taegukgi', 'Joe Black'];
+
+                favMovies.shift(0);
+                favMovies.push('Interstellar', 'Gladiator');
+            
+
+                if(i=0, i<favMovies.length-1, i++){
+                    console.log("One of my favorite movies is " + favMovies[i])
+                };
+                
